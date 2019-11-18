4f35f9d06e9831decc183694f0fe42b0e653fca7
Radomir Zekovik <liquidraso@gmail.com>
2019-11-18 17:07:07 +0100

    User Model done

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  has_many :dogs
+  has_many :meeting, through: :dogs
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :address, presence: true
+end
