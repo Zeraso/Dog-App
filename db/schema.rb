20f33924b3c76524fa0957743d97237db476be27
Karim Abouchalache <kfa4vw@virginia.edu>
2019-11-19 15:11:53 +0100

    Model of Meeting created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_19_100631) do
+ActiveRecord::Schema.define(version: 2019_11_19_135016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,17 @@ ActiveRecord::Schema.define(version: 2019_11_19_100631) do
     t.string "gender"
     t.date "birthday"
     t.index ["user_id"], name: "index_dogs_on_user_id"
+  end
+
+  create_table "meetings", force: :cascade do |t|
+    t.date "date"
+    t.text "comment"
+    t.string "status"
+    t.integer "rating"
+    t.bigint "dog_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["dog_id"], name: "index_meetings_on_dog_id"
   end
 
   create_table "users", force: :cascade do |t|
