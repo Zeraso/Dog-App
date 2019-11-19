33c5ebacf4fcaa8441f2e2cab7b8c7ca43a537cc
Karim Abouchalache <kfa4vw@virginia.edu>
2019-11-19 17:54:34 +0100

    Added show to controller meeting

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,5 +1,24 @@
 class MeetingsController < ApplicationController
+  before_action :set_meeting, only: [:show, :edit, :update, :destroy]
+
   def index
     @meetings = policy_scope(Meeting).order(created_at: :desc)
+  end
+
+  def show
+  end
+
+
+
+
+   private
+
+  def meeting_params
+    params.require(:meeting).permit(:date, :comment, :status, :rating)
+  end
+
+  def set_meeting
+    @meeting = Meeting.find(params[:id])
+    authorize @meeting
   end
 end
