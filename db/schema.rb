236cb1129b7e87268b80062f0d6c463cccb17013
Radomir Zekovik <liquidraso@gmail.com>
2019-11-20 14:36:59 +0100

    dsv

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_11_20_104201) do
 
   # These are extensions that must be enabled in order to support this database
@@ -55,8 +54,6 @@ ActiveRecord::Schema.define(version: 2019_11_20_104201) do
     t.string "first_name"
     t.string "address"
     t.string "status"
-    t.float "latitude"
-    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
