f954430a228d614a624e98c05782d7c27d010036
Karim Abouchalache <kfa4vw@virginia.edu>
2019-11-21 12:01:17 +0100

    Meeting back done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-User.new(email: "test@gmail.com", encrypted_password: "AZERTY", last_name: "Uber", first_name: "Blanquette de Veau", address: "16 villa Gaudelet Paris")
-Dog.new(name: "Bobby", gender: "Male", breed: "pussy", coat: "Pink")
-
 require 'faker'
 
+puts "Destroying all my meetings"
+Meeting.destroy_all
+puts "Destroying all my users"
+User.destroy_all
 puts "Destroying all my dogs"
 Dog.destroy_all
 
+User.create!(email: "k@hotmail.com", password: "AZERTY", last_name: "Uber", first_name: "Blanquette de Veau", address: "16 villa Gaudelet Paris")
 
 puts 'Creating 20 doggies...'
 20.times do
@@ -22,7 +17,7 @@ puts 'Creating 20 doggies...'
     birthday: "2018""12""12",
     gender: ["Female","Male"].sample,
     coat:Faker::Creature::Dog.coat_length,
-    user_id: User.first
+    user: User.first
   )
   dog.save!
 end
